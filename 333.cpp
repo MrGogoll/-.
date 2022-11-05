84a134b9da33c58e91910c189905c401addc2c7f
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-05 18:20:41 +0300

    вывести все четные числа в порядке наоборот.

---
+++ 333.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int mass[100];
+    int N= 100;
+	for (int i = 0; i < 51; i++)
+	{
+		if (N % 2 == 0) {
+			cout << N-2 * i<<endl  ;
+		}
+	}
+}
+
